4e375e7d5ecb8d8d3fcf9c4d290e7902301b4898
Afzal-1720 <f.mdafzal206@gmail.com>
2026-06-28 21:58:39 +0530

    Hands On 3 completed

--- Week 2/Module 5 - Python Backend Frameworks/coursemanager/courses/urls.py
+++ Week 2/Module 5 - Python Backend Frameworks/coursemanager/courses/urls.py
@@ -1,6 +1,49 @@
-from django.urls import path
+"""from django.urls import path
 from . import views
 
+from django.urls import path
+from .views import CourseListView, CourseDetailView
+
 urlpatterns = [
-    path('hello/', views.hello_view, name='hello'),
-]
+
+    path(
+        "courses/",
+        CourseListView.as_view(),
+        name="course-list"
+    ),
+
+    path(
+        "courses/<int:pk>/",
+        CourseDetailView.as_view(),
+        name="course-detail"
+    ),
+
+]"""
+
+from rest_framework.routers import DefaultRouter
+from .views import (
+CourseViewSet,
+StudentViewSet,
+EnrollmentViewSet
+)
+
+
+router = DefaultRouter()
+
+
+router.register(
+r'courses',
+CourseViewSet
+)
+
+router.register(
+r'students',
+StudentViewSet
+)
+
+router.register(
+r'enrollments',
+EnrollmentViewSet
+)
+
+urlpatterns = router.urls
--- Week 2/Module 5 - Python Backend Frameworks/coursemanager/courses/views.py
+++ Week 2/Module 5 - Python Backend Frameworks/coursemanager/courses/views.py
@@ -1,7 +1,123 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
-from django.http import HttpResponse
+from .models import Course, Enrollment, Student
+from .serializers import CourseSerializer, EnrollmentSerializer, StudentSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
-def hello_view(request):
-    return HttpResponse("Course Management API is running")
+
+"""class CourseListView(APIView):
+
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
+    
+class CourseDetailView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Course.objects.get(pk=pk)
+        except Course.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        course = self.get_object(pk)
+
+        if course is None:
+            return Response(
+                {"error": "Course not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        course = self.get_object(pk)
+
+        if course is None:
+            return Response(
+                {"error": "Course not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = CourseSerializer(course, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        course = self.get_object(pk)
+
+        if course is None:
+            return Response(
+                {"error": "Course not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        course.delete()
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+        """
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(detail=True, methods=["get"])
+    def students(self, request, pk=None):
+        # Get the current course
+        course = self.get_object()
+
+        # Get all enrollments for this course
+        enrollments = course.enrollment_set.all()
+
+        # Extract students
+        students = [enrollment.student for enrollment in enrollments]
+
+        # Convert to JSON
+        serializer = StudentSerializer(students, many=True)
+
+        return Response(serializer.data)
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    @action(detail=True, methods=["get"])
+    def courses(self, request, pk=None):
+        student = self.get_object()
+
+        enrollments = student.enrollment_set.all()
+
+        courses = [e.course for e in enrollments]
+
+        serializer = CourseSerializer(courses, many=True)
+
+        return Response(serializer.data)
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
